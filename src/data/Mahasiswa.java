575a768cdc0a727194aab557359c15d2ded3af32
Muhammad Maulud H. R. <mmauludhr@gmail.com>
2017-04-30 10:25:50 +0700

    Adding Fresh Line of Code :D

--- src/data/Mahasiswa.java
+++ src/data/Mahasiswa.java
@@ -1,8 +1,8 @@
 /*
- * Tugas Besar - Pemrograman Berorientasi Objek
- * Muhammad Maulud H. R. | 1301154166
- * M. Widadio Ilham	  | 1301154360
- * Afra W. D.	  | 1301150432
+ * Tugas Besar_Pemrograman Berorientasi Objek_Aplikasi E-Learning
+ * Muhammad Maulud H. R.    | 1301154166
+ * M. Widadio Ilham         | 1301154360
+ * Afra W. D.               | 1301150432
  */
 package data;
 
@@ -10,6 +10,24 @@ package data;
  *
  * @author M Maulud H R
  */
-public class Mahasiswa {
+public class Mahasiswa extends Orang{
+    private String NIM;
+
+    public Mahasiswa(String nama, String tgl_lahir, String tmpt_lahir, String NIM){
+        super.setNama(nama);
+        super.setTgl_lahir(tgl_lahir);
+        super.setTmpt_lahir(tmpt_lahir);
+        this.NIM = NIM;
+    }
+    
+    public String getNIM() {
+        return NIM;
+    }
+
+    @Override
+    public String pekerjaan() {
+        return "Mahasiswa";
+    }
+    
     
 }
--- src/data/Orang.java
+++ src/data/Orang.java
@@ -1,8 +1,8 @@
 /*
- * Tugas Besar - Pemrograman Berorientasi Objek
- * Muhammad Maulud H. R. | 1301154166
- * M. Widadio Ilham	  | 1301154360
- * Afra W. D.	  | 1301150432
+ * Tugas Besar_Pemrograman Berorientasi Objek_Aplikasi E-Learning
+ * Muhammad Maulud H. R.    | 1301154166
+ * M. Widadio Ilham         | 1301154360
+ * Afra W. D.               | 1301150432
  */
 package data;
 
@@ -10,6 +10,54 @@ package data;
  *
  * @author M Maulud H R
  */
-public class Orang {
+public abstract class Orang {
+    private String nama;
+    private String tgl_lahir;
+    private int umur;
+    private String tmpt_lahir;
+    private String alamat;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTgl_lahir() {
+        return tgl_lahir;
+    }
+
+    public void setTgl_lahir(String tgl_lahir) {
+        this.tgl_lahir = tgl_lahir;
+    }
+
+    public int getUmur() {
+        //Perhitungan denga tipe data "Date". Tanggal sekarang - tanggal lahir = umur.
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+
+    public String getTmpt_lahir() {
+        return tmpt_lahir;
+    }
+
+    public void setTmpt_lahir(String tmpt_lahir) {
+        this.tmpt_lahir = tmpt_lahir;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    
+    public abstract String pekerjaan();
     
 }
