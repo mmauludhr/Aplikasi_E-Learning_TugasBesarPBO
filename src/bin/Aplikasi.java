8c9e987c5c90640dac8edf83cf07e1d435c80821
Muhammad Maulud H R <Muhammad Maulud H R@DESKTOP-DEQE567>
2017-05-02 01:10:55 +0700

    File Elva

--- src/bin/Aplikasi.java
+++ src/bin/Aplikasi.java
@@ -1,8 +1,8 @@
 /*
- * Tugas Besar - Pemrograman Berorientasi Objek
- * Muhammad Maulud H. R. | 1301154166
- * M. Widadio Ilham	  | 1301154360
- * Afra W. D.	  | 1301150432
+ * Tugas Besar_Pemrograman Berorientasi Objek_Aplikasi E-Learning
+ * Muhammad Maulud H. R.    | 1301154166
+ * M. Widadio Ilham         | 1301154360
+ * Afra W. D.               | 1301150432
  */
 package bin;
 
